0a892edfbec99540dfb541dfffc093702a142162
Peter Mühleder <peter.muehleder@uni-leipzig.de>
2019-02-26 15:06:49 +0100

    directory option added to commenter command

--- commenters.py
+++ commenters.py
@@ -1,14 +1,21 @@
 import click
+import os
 
 from yats.commenter_dataset import build_commenter_dataset
 
 
 @click.command()
-@click.argument("archive_filepath")
+@click.option("--archive", "-a", multiple=True)
+@click.option("--directory", "-d")
 @click.option("--out_dir", "-o", default="data")
 @click.option("--filename", "-f", default="commentor_dataset")
-def build_dataset(archive_filepath, out_dir, filename):
-    build_commenter_dataset(archive_filepath, out_dir, filename)
+def build_dataset(archive, directory, out_dir, filename):
+    if directory:
+        files = os.listdir(directory)
+        archive = [ os.path.join(directory, x) for x in files if x.endswith(".zip") ] 
+
+
+    build_commenter_dataset(archive, out_dir, filename)
 
 if __name__ == '__main__':
     build_dataset()
--- yats/commenter_dataset.py
+++ yats/commenter_dataset.py
@@ -3,17 +3,15 @@ import sys
 import requests
 from .zip_archive import ZipArchive
 from pyg.reader import YoutubeArchiveReader
+from .config import YAC_BASE
 
 def api_call(url):
     rsp = requests.get(url)
     return rsp.json()
 
 
-def build_commenter_dataset(archive_filepath, out_dir, filename):
+def build_commenter_dataset(archive_filepaths, out_dir, filename):
 
-    if not os.path.exists(archive_filepath):
-        print("youtube archive {} does not exist".format(archive_filepath))
-        sys.exit(1)
 
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
@@ -21,25 +19,30 @@ def build_commenter_dataset(archive_filepath, out_dir, filename):
     out_filepath = os.path.join(out_dir, filename)
     out_archive = ZipArchive(out_filepath)
 
-    archive = YoutubeArchiveReader(archive_filepath)
-    user_ids = set()
-    for video in archive:
-        user_ids.update([ c["author_id"] for c in video.comments ])
 
-    print(len(user_ids))
+    for archive_filepath in archive_filepaths:
+        if not os.path.exists(archive_filepath):
+            print("youtube archive {} does not exist".format(archive_filepath))
+            sys.exit(1)
 
-    for i, user_id in enumerate(user_ids):
-        print("{}/{}".format(i,len(user_ids)))
+        archive = YoutubeArchiveReader(archive_filepath)
+        user_ids = set()
+        for video in archive:
+            user_ids.update([ c["author_id"] for c in video.comments ])
 
-        filepath = "{}.json".format(user_id)
-        if not out_archive.contains(filepath):
+        print(len(user_ids))
 
-            metadata = api_call("http://localhost:5000/channel/{}".format(user_id))
-            related_channels = api_call("http://localhost:5000/channel/{}/related".format(user_id))
-            
-            
-            data = {
-                "metadata": metadata,
-                "related_channels": related_channels
-            }
-            out_archive.add(filepath, data)    
+        for i, user_id in enumerate(user_ids):
+            print("{}/{}".format(i,len(user_ids)))
+
+            filepath = "{}.json".format(user_id)
+            if not out_archive.contains(filepath):
+
+                metadata = api_call(YAC_BASE+"/channel/{}".format(user_id))
+                related_channels = api_call(YAC_BASE+"/channel/{}/related".format(user_id))
+                
+                data = {
+                    "metadata": metadata,
+                    "related_channels": related_channels
+                }
+                out_archive.add(filepath, data)    
